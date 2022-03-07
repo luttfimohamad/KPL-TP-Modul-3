d9e499326ed8d508b911c221e76594e08e2ad60a
luttfimohamad <luttfimohamad@gmail.com>
2022-03-07 23:20:23 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul3_1302204066/KodePos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_1302204066
+{
+    internal class KodePos
+    {
+        public string getKodePos(string kelurahan)
+        {
+            if (kelurahan == "Batununggal")
+            {
+                return "40266";
+            }
+            else if (kelurahan == "Kujangsari")
+            {
+                return "40287";
+            }
+            else if (kelurahan == "Mengger")
+            {
+                return "40267";
+            }
+            else if (kelurahan == "Wates")
+            {
+                return "40256";
+            }
+            else if (kelurahan == "Cijaura")
+            {
+                return "40287";
+            }
+            else if (kelurahan == "Jatisari")
+            {
+                return "40286";
+            }
+            else if (kelurahan == "Margasari")
+            {
+                return "40286";
+            }
+            else if (kelurahan == "Sekejati")
+            {
+                return "40286";
+            }
+            else if (kelurahan == "Kebonwaru")
+            {
+                return "40272";
+            }
+            else if (kelurahan == "Maleer")
+            {
+                return "40274";
+            }
+            else if (kelurahan == "Samoja")
+            {
+                return "40273";
+            }
+            return null;
+        }
+    }
+}
--- tpmodul3_1302204066/Program.cs
+++ tpmodul3_1302204066/Program.cs
@@ -3,4 +3,19 @@ Nama        : Mohamad Lutfi
 NIM         : 1302204066
 Kelas       : SE-44-02
  */
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace tpmodul3_1302204066
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inputkan nama kelurahan: ");
+            string i = Console.ReadLine();
+            KodePos kodePos = new KodePos();
+            Console.WriteLine("Kode Pos: " + kodePos.getKodePos(i) + "\n");
+        }
+    }
+}

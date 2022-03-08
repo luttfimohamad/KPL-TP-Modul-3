ed5fc45e66083d54e43f95a3e56da50a8fc831dd
luttfimohamad <luttfimohamad@gmail.com>
2022-03-08 11:52:24 +0700

    menambahkan implementasi dengan state-based construction

---
+++ tpmodul3_1302204066/DoorMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_1302204066
+{
+    internal class DoorMachine
+    {
+        public DoorMachine()
+        {
+            Console.WriteLine("Pintu terkunci");
+        }
+        public void KunciPintu()
+        {
+            Console.WriteLine("Pintu terkunci");
+        }
+        public void BukaPintu()
+        {
+            Console.WriteLine("Pintu tidak terkunci");
+        }
+    }
+}
--- tpmodul3_1302204066/Program.cs
+++ tpmodul3_1302204066/Program.cs
@@ -12,18 +12,45 @@ namespace tpmodul3_1302204066
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("------------ MENCARI KODEPOS KELURAHAN ------------" + "\n");
+            Console.WriteLine("------------ MENCARI KODEPOS KELURAHAN ------------");
             Console.WriteLine("Daftar kelurahan yang terssedia: ");
             Console.WriteLine("1. Batununggal   " + " 7. Margasari");
             Console.WriteLine("2. Kujangsari    " + " 8. Sekejati");
             Console.WriteLine("3. Mengger       " + " 9. Kebonwaru");
             Console.WriteLine("4. Wates         " + " 10. Maleer");
             Console.WriteLine("5. Cijaura       " + " 11. Samoja");
-            Console.WriteLine("6. Jatisari" + "\n");
-            Console.Write("Inputkan nama kelurahan: ");
+            Console.WriteLine("6. Jatisari");
+
+            Console.Write("Masukkan nama kelurahan: ");
             string masukan = Console.ReadLine();
+            if (masukan == "")
+            {
+                Console.Write("*Anda tidak memasukkan nama kelurahan" + "\n");
+            }
+
             KodePos kodePos = new KodePos();
-            Console.WriteLine("Kode Pos: " + kodePos.getKodePos(masukan));
+            Console.WriteLine("Kode Pos: " + kodePos.getKodePos(masukan) + "\n");
+
+            Console.WriteLine("------------------ MESIN PINTU ------------------");
+            Console.Write("Kondisi saat ini: ");
+            DoorMachine pintu = new DoorMachine();
+
+            Console.Write("Masukkan aksi (Buka/Kunci): ");
+            String masukan2 = Console.ReadLine();
+            if (masukan2 == "Buka")
+            {
+                Console.Write("Kondisi saat ini: ");
+                pintu.BukaPintu();
+            }
+            else if (masukan2 == "Kunci")
+            {
+                Console.Write("Kondisi saat ini: ");
+                pintu.KunciPintu();
+            }
+            else
+            {
+                Console.Write("*Aksi yang anda masukkan salah" + "\n");
+            }
         }
     }
 }
